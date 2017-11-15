72426593fa0492900c2b0fb691e0a0c112529154
shenke <2500639734@qq.com>
2017-11-15 16:04:01 +0800

    update

--- bing-bbs/bing-bbs-web/src/main/java/com/bing/bbs/controller/test/TestController.java
+++ bing-bbs/bing-bbs-web/src/main/java/com/bing/bbs/controller/test/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bing.bbs.pojo.test.Test;
 import com.bing.bbs.service.test.TestService;
+import com.bing.common.util.HttpClientUtils;
 
 @RequestMapping("/test")
 @Controller
@@ -17,8 +18,15 @@ public class TestController {
 	
 	@RequestMapping("/save")
 	@ResponseBody
-	public String save() throws Exception {
+	public String save() {
 		return testService.saveTest(new Test()) + "";
+	}
+	
+	@RequestMapping("/redirection")
+	@ResponseBody
+	public void redirection() {
+		String doGet = HttpClientUtils.doGet("http://192.168.2.140:8082/activiti/createModel/cs/1/csD");
+		System.out.println(doGet);
 	}
 
 }
