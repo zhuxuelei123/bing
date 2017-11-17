eae948b782aaf5f6cffa472984ff22b85e4f827e
zhuxuelei123 <1075883021@qq.com>
2017-11-18 03:08:33 +0800

    sso项目搭建

---
+++ bing-sso/bing-sso-service/src/main/java/com/bing/sso/service/test/TestUserFindService.java
@@ -0,0 +1,5 @@
+package com.bing.sso.service.test;
+
+public interface TestUserFindService {
+	public void findUserById();
+}
---
+++ bing-sso/bing-sso-service/src/main/java/com/bing/sso/service/test/TestUserService.java
@@ -0,0 +1,5 @@
+package com.bing.sso.service.test;
+
+public interface TestUserService {
+	public void saveUser();
+}
